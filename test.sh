b9b05e099cf0c3de072189e5c315df1a92245599
sebastien peterson boudreau <sebastien.peterson.boudreau@gmail.com>
2024-12-28 05:32:46 -0400

    Update test to use variable expansions
    there is a weird bug with undefined variables. originally this test had
    '{{ foo }} {{bar}}', but that was making it print junk memory. i assume
    its some issue in 'lookup' looking up 'bar', but im not sure and im not
    sure how to fix it......

--- test.sh
+++ test.sh
@@ -8,15 +8,19 @@ make res &&
 @foo bar
 foo {{ ls }} bar
 quux
+{{ foo }}
 {{ ls }} baz
 @bar foo
+{{bar }}
 EOF
 
 printf '%s' 'foo ' > expected
 ls >> expected
 printf '%s\n' ' bar' >> expected
 printf '%s\n' 'quux' >> expected
+printf '%s\n' 'bar' >> expected
 ls >> expected
 printf '%s\n' ' baz' >> expected
+printf '%s\n' 'foo' >> expected
 
 cmp actual expected && echo 'pass' || echo 'fail'
